0eecd470c7ff72ff69528c60530f90bcbd4b2337
Luis Martinez <luis_mmartinez@live.com>
2021-07-08 23:09:58 -0400

    commit tree a0166d4a2438bd8b2571fd64c6e1753676d86843

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, { Component } from 'react'
+
+
+export default class App extends Component {
+    state = {
+        poepleInSpace: []
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.poepleInSpace.map(person => person.name)}
+            </div>
+        )
+    }
+
+    componentDidMount(){
+        fetch("http://api.open-notify.org/astros.json")
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                poepleInSpace: data.people
+            })
+        })
+    }
+
+}
